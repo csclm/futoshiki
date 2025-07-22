0ad07166d063272651eec0f2e45b8e5483ebe12c
Nick <9688487+csclm@users.noreply.github.com>
2025-07-21 21:59:55 -0500

    Initial commit

---
+++ futoshiki.py
@@ -0,0 +1,162 @@
+from level import *
+
+def rowsAndCols(level: WorkingLevel):
+    for row in level.grid:
+        yield row
+    dimension = len(level.grid)
+    for col in range(dimension):
+        yield [level.grid[row][col] for row in range(dimension)]
+
+def isLevelValid(level: WorkingLevel) -> bool:
+    for rowCol in rowsAndCols(level):
+        determined = set()
+        possibilities = { num: 0 for num in range(1, len(level.grid)+1)}
+        for square in rowCol:
+            if len(square) == 1:
+                determined.add(next(iter(square)))
+            for num in square:
+                possibilities[num] += 1
+        for num, poss in possibilities.items():
+            if poss == 0:
+                return False
+            if poss > 1 and num in determined:
+                return False
+    for inequality in level.inequalities:
+        lowSquare = level.grid[inequality.low[0]][inequality.low[1]]
+        highSquare = level.grid[inequality.high[0]][inequality.high[1]]
+        if len(lowSquare) == 1 and len(highSquare) == 1:
+            if next(iter(lowSquare)) >= next(iter(highSquare)):
+                return False
+    return True
+
+##
+# No row or column can have a duplicate of a number
+# Remove possibilities that suggest this
+##
+def applyExclusionRule(level: WorkingLevel) -> bool:
+    didChange = False
+    for rowCol in rowsAndCols(level):
+        determined = [next(iter(square)) for square in rowCol if len(square) == 1]
+        for square in rowCol:
+            if len(square) == 1:
+                continue
+            for value in determined:
+                if value in square:
+                    square.discard(value)
+                    didChange = True
+    return didChange
+
+##
+# Each row or column must have at least one occurrence of a number
+# If a row or column has only one way of containing that number, that must be the way toward the solution
+##
+def applySelectionRule(level: WorkingLevel) -> bool:
+    didChange = False
+    topNum = len(level.grid[0])
+    #row wise
+    for rowCol in rowsAndCols(level):
+        possibilityCount = { num : 0 for num in range(1, topNum+1)}
+        for square in rowCol:
+            for possibility in square:
+                possibilityCount[possibility] += 1
+        for possibility, count in possibilityCount.items():
+            if count != 1:
+                continue
+            for square in rowCol:
+                if possibility in square and len(square) > 1:
+                    didChange = True
+                    square.clear()
+                    square.add(possibility)
+    return didChange
+
+##
+# Inequality rule part 1:
+# The lowest possibility on the low side of an inequality must be lower than
+# the lowest possibility on the high side of the inequality
+##
+def applyLowerBoundRule(level: WorkingLevel) -> bool:
+    didChange = False
+    for inequality in level.inequalities:
+        lowSquare = level.grid[inequality.low[0]][inequality.low[1]]
+        highSquare = level.grid[inequality.high[0]][inequality.high[1]]
+        for lowNum in range(1,min(lowSquare)+1):
+            # remove all from the high square which are <= the minimum of the low square
+            if lowNum in highSquare:
+                highSquare.discard(lowNum)
+                didChange = True
+    return didChange
+
+##
+# Inequality rule part 2:
+# The highest possibility on the high side of an inequality must be higher than
+# the highest possibility on the low side of the inequality
+##
+def applyUpperBoundRule(level: WorkingLevel):
+    didChange = False
+    topNum = len(level.grid[0])
+    for inequality in level.inequalities:
+        lowSquare = level.grid[inequality.low[0]][inequality.low[1]]
+        highSquare = level.grid[inequality.high[0]][inequality.high[1]]
+        for highNum in range(max(highSquare),topNum+1):
+            #remove all from the high square which are <= the minimum of the low square
+            if highNum in lowSquare:
+                lowSquare.discard(highNum)
+                didChange = True
+    return didChange
+
+def applyRules(workingLevel: WorkingLevel):
+    return \
+        applyExclusionRule(workingLevel) or \
+        applyLowerBoundRule(workingLevel) or \
+        applyUpperBoundRule(workingLevel) or \
+        applySelectionRule(workingLevel)
+
+# Generates versions of the level with a single possibility removed
+def validMutations(workingLevel: WorkingLevel):
+    for row in range(len(workingLevel.grid)):
+        for col in range(len(workingLevel.grid)):
+            for num in range(1, len(workingLevel.grid)+1):
+                if num not in workingLevel.grid[row][col]:
+                    continue
+                nextIteration = workingLevel.copy()
+                nextIteration.grid[row][col].discard(num)
+                if isLevelValid(nextIteration):
+                    yield nextIteration
+
+def solveWorkingLevel(workingLevel: WorkingLevel, depth: int = 0) -> bool:
+    print("Solving, depth = ", depth)
+    appliedAnyRules = False
+    while applyRules(workingLevel):
+        # applyRules affects workingLevel and
+        # returns false when no rules apply
+        appliedAnyRules = True
+    if not appliedAnyRules:
+        return False
+    if not isLevelValid(workingLevel):
+        return False
+    if isLevelComplete(workingLevel):
+        return True
+
+    # Here, we've reached a state where our rules can't advance us
+    # further, so make a random mutation and try to keep solving
+    for nextIteration in validMutations(workingLevel):
+        if solveWorkingLevel(nextIteration, depth+1):
+            workingLevel.grid = nextIteration.grid
+            return True
+    return False
+
+
+print("Enter level in fufen notation")
+fufen = input()
+level = parseFuFen(fufen)
+print("Parsed Level:")
+workingLevel = createWorkingLevel(level)
+print()
+didSolve = solveWorkingLevel(workingLevel)
+
+if didSolve:
+    print("Solution:")
+    printCompletedLevel(workingLevel)
+else:
+    print("Stuck!")
+    printIncompleteWorkingLevel(workingLevel)
---
+++ level.py
@@ -0,0 +1,128 @@
+from dataclasses import dataclass
+from typing import List, Set, Tuple
+
+@dataclass
+class Inequality:
+    # Row major coords
+    high: Tuple[int,int]
+    low: Tuple[int,int]
+
+@dataclass
+class ParsedLevel:
+    grid: List[List[str]]
+    inequalities: List[Inequality]
+
+# Examples:
+# .>351./.2.5./.....^/.5.3./.432<.
+# ....7..<.<./.V...3.>..^./..^>.7.9.../..7...<6..^/3V7..V..V.8V9/.^.^3^..^<.7../...3.2.V.<.V/..^.<.8...>./.^..V.6.>..^.
+# ..>.>..<..../.>..>.2V..<../6^...>..>.^.9/9.V.<.V<....2/..>.^....V.^./2.<...V..V.>5/3<.V..>.>.>..8/...V.3.>..V./.>.<...^<.V.V.>.^
+def parseFuFen(fufen: str) -> ParsedLevel:
+    lines = []
+    inequalities = []
+    currentLine = []
+    for char in fufen:
+        if char == '/':
+            lines.append(currentLine)
+            currentLine = []
+        elif char == "." or char.isdigit():
+            currentLine.append(char)
+        elif char == "^":
+            inequalities.append(Inequality((len(lines), len(currentLine)-1),(len(lines)-1, len(currentLine)-1)))
+        elif char == "V":
+            inequalities.append(Inequality((len(lines)-1, len(currentLine)-1),(len(lines), len(currentLine)-1)))
+        elif char == "<":
+            inequalities.append(Inequality((len(lines), len(currentLine)),(len(lines), len(currentLine)-1)))
+        elif char == ">":
+            inequalities.append(Inequality((len(lines), len(currentLine)-1),(len(lines), len(currentLine))))
+    if len(currentLine):
+        lines.append(currentLine)
+
+    # assert this is square
+    height = len(lines)
+    width = len(lines[0])
+    for line in lines:
+        if len(line) != width:
+            raise ValueError("Grid is jagged")
+    if width != height:
+            raise ValueError("Grid is rectangular")
+
+    #assert all the inequalities are valid indices
+    for inequality in inequalities:
+        if not (\
+            inequality.low[0] in range(height) and \
+            inequality.high[0] in range(height) and \
+            inequality.low[1] in range(width) and \
+            inequality.high[1] in range(width) \
+        ):
+            raise ValueError(f"Inequality outside of grid {inequality}")
+    return ParsedLevel(lines, inequalities)
+
+@dataclass
+class WorkingLevel:
+    grid: List[List[Set[int]]]
+    inequalities: List[Inequality]
+
+    def copy(self):
+        return WorkingLevel([[s.copy() for s in row] for row in self.grid], self.inequalities)
+
+def createWorkingLevel(level: ParsedLevel) -> WorkingLevel:
+    topValue = len(level.grid[0])
+    def possibilitiesForSquare(square):
+        if square == '.':
+            return set(range(1,topValue+1)) 
+        else:
+            return set([int(square)])
+    return WorkingLevel([[possibilitiesForSquare(square, topValue) for square in row] for row in level.grid], level.inequalities)
+
+def printIncompleteWorkingLevel(level: WorkingLevel):
+    dimension = len(level.grid)
+    charDimension = dimension*4+1
+    charGrid = []
+    for _ in range(charDimension):
+        charGrid.append([" "]*charDimension)
+
+    # paint lines
+    for row in range(dimension):
+        for col in range(dimension+1): 
+            charGrid[row*4] = list("\u251c" + "\u253c".join(["\u2500"*3]*dimension) + "\u2524")
+        for subRow in range(3):
+            for col in range(dimension+1):
+                charGrid[row*4+subRow+1][col*4] = "\u2502"
+    charGrid[0] = list("\u250c" + "\u252c".join(["\u2500"*3]*dimension) + "\u2510")
+    charGrid[-1] = list("\u2514" + "\u2534".join(["\u2500"*3]*dimension) + "\u2518")
+
+    # paint possibilities
+    for row in range(dimension):
+        for col in range(dimension):
+            for i in range(1, dimension+1):
+                if i in level.grid[row][col]:
+                    charGrid[row*4+1+(i-1)//3][col*4+1+(i-1)%3] = str(i)
+
+    # paint inequalities
+    for inequality in level.inequalities:
+        char = ""
+        if inequality.high[0] > inequality.low[0]:
+            char = "^"
+        if inequality.high[0] < inequality.low[0]:
+            char = "v"
+        if inequality.high[1] > inequality.low[1]:
+            char = "<"
+        if inequality.high[1] < inequality.low[1]:
+            char = ">"
+        row = (inequality.high[0] + inequality.low[0])/2
+        col = (inequality.high[1] + inequality.low[1])/2
+        charGrid[int(row*4)+2][int(col*4)+2] = char
+    
+    for line in charGrid:
+        print("".join(line))
+
+def printCompletedLevel(level: WorkingLevel):
+    for line in level.grid:
+        print("".join((str(next(iter(square))) for square in line)))
+    
+def isLevelComplete(level: WorkingLevel):
+    for line in level.grid:
+        for square in line:
+            if len(square) != 1:
+                return False
+    return True
